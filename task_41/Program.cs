4306f0df3dfd27506577d0c9bd8512fe1ff00fc9
Ekaterina <peka29@yandex.ru>
2023-03-29 22:07:11 +0300

    add task_41

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -2,3 +2,7 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
